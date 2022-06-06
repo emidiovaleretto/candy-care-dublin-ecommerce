2930332ae0ba35f7a58e6e12685350bcfa01ef9c
Emidio Valereto <emidio.valereto@gmail.com>
2022-06-06 20:14:53 +0100

    Change environment for deploy.

--- candycaredublin/wsgi.py
+++ candycaredublin/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'candycaredublin.settings.development')
+                      'candycaredublin.settings.production')
 
 application = get_wsgi_application()
