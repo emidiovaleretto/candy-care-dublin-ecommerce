81822a0134b1a3ad5c4b2fa50c4b6d4a03957084
Emidio Valereto <emidio.valereto@gmail.com>
2022-06-06 20:40:57 +0100

    Set environment vars for production.

--- candycaredublin/settings/production.py
+++ candycaredublin/settings/production.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .settings import *
 
 if os.path.exists("env.py"):
@@ -6,3 +7,8 @@ if os.path.exists("env.py"):
 
 DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY_PRODUCTION")
+ALLOWED_HOSTS = [os.environ.get("HEROKU_HOST")]
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("HEROKU_DB"))
+}
