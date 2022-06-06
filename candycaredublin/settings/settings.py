28b04515945e67019d5f0756d6a99b5560cb10cb
Emidio Valereto <emidio.valereto@gmail.com>
2022-06-06 20:46:57 +0100

    Set the staticfiles storage.

--- candycaredublin/settings/settings.py
+++ candycaredublin/settings/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
