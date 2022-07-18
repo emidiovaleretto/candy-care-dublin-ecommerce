d02b8c452afa3fa608b5e1532e4071a1979ea011
Emidio Valeretto <emidio.valereto@gmail.com>
2022-07-18 12:57:01 +0000

    Change products url name.

--- candycaredublin/urls.py
+++ candycaredublin/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('products/', include('products.urls')),
+    path('shop/', include('products.urls')),
 ]
