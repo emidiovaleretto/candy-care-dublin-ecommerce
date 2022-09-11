7a65ef6c18bf091636c59e8510c023b08d6de0c0
Emidio Valeretto <emidio.valereto@gmail.com>
2022-09-11 13:02:53 +0000

    Add User Models tests.

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestModelsUser(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username='test',
+            password='12test12',
+            email='test@test.com'
+        )
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_create_new_user(self):
+        user = User(
+            username='test',
+            password='12test12',
+            email='test@test.com'
+        )
+        self.assertTrue(user)
+        self.assertIsInstance(user, User)
+        self.assertEqual(1, User.objects.count())
+        self.assertIsNotNone(user)
