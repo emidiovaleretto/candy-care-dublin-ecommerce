436e607847b44fdf65a3143e172b9fb83492a8ed
Emidio Valeretto <emidio.valereto@gmail.com>
2022-09-11 12:26:23 +0000

    Add Category tests.

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Category, Product
 
-# Create your tests here.
+
+class TestModelsCategory(TestCase):
+
+    def test_create_new_product_category(self):
+        Category.objects.create(name='Category 1')
+        self.assertTrue(Category.objects.exists())
+
+    def test_str_name_repr(self):
+        entry = Category(name='Category 1')
+        self.assertEqual(str(entry), entry.name)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Category._meta.verbose_name_plural), "Categories")
