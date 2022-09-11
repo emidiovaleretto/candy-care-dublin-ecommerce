ce537f48d2e30feffa23a79d7e3da18001094102
Emidio Valeretto <emidio.valereto@gmail.com>
2022-09-11 12:29:49 +0000

    Add model Products tests.

--- products/tests.py
+++ products/tests.py
@@ -14,3 +14,26 @@ class TestModelsCategory(TestCase):
 
     def test_verbose_name_plural(self):
         self.assertEqual(str(Category._meta.verbose_name_plural), "Categories")
+
+
+class TestModelsProduct(TestCase):
+
+    def test_create_new_product(self):
+        Product.objects.create(
+            name='Product 1',
+            description='Testing Model Products',
+            price=100
+        )
+        self.assertTrue(Product.objects.exists())
+
+    def test_str_name_repr(self):
+        entry = Category(name='Category 1')
+        self.assertEqual(str(entry), entry.name)
+
+    def test_instance_product(self):
+        product = Product.objects.create(
+            name='Product 1',
+            description='Testing Model Products',
+            price=100
+        )
+        self.assertIsInstance(product, Product)
