a8226c0a474a59b079aa0a6986f4eebd0b832d79
Emidio Valeretto <emidio.valereto@gmail.com>
2022-09-11 11:58:14 +0000

    Add home tests.

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestViewHomeIndex(TestCase):
+
+    def test_simple_index_must_return_200(self):
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code)
+
+
+    def test_load_index_template(self):
+        self.assertTemplateUsed(template_name='index.html')
+
+    
+    def test_page_not_found_must_return_404(self):
+        response = self.client.get('/home')
+        self.assertEqual(404, response.status_code)
