6d75bed1c1614aec2d3c1dccdad403a1166c0b5c
Emidio Valeretto <emidio.valereto@gmail.com>
2022-09-13 11:17:09 +0000

    Refactor tests of the Product Models.

--- products/tests.py
+++ products/tests.py
@@ -4,36 +4,44 @@ from .models import Category, Product
 
 class TestModelsCategory(TestCase):
 
+    def setUp(self):
+        self.category = Category.objects.create(name='Category 1')
+        self.category.save()
+
+    def tearDown(self):
+        self.category.delete()
+
     def test_create_new_product_category(self):
-        Category.objects.create(name='Category 1')
-        self.assertTrue(Category.objects.exists())
+        self.assertTrue(self.category is not None)
+        self.assertEqual(1, Category.objects.count())
+        self.assertIsInstance(self.category, Category)
 
     def test_str_name_repr(self):
-        entry = Category(name='Category 1')
-        self.assertEqual(str(entry), entry.name)
+        self.assertEqual(str(self.category), self.category.name)
 
     def test_verbose_name_plural(self):
-        self.assertEqual(str(Category._meta.verbose_name_plural), "Categories")
+        self.assertEqual(str(self.category._meta.verbose_name_plural), "Categories")
 
 
 class TestModelsProduct(TestCase):
 
-    def test_create_new_product(self):
-        Product.objects.create(
+    def setUp(self):
+        self.product = Product.objects.create(
             name='Product 1',
             description='Testing Model Products',
             price=100
         )
-        self.assertTrue(Product.objects.exists())
+        self.product.save()
+
+    def tearDown(self):
+        self.product.delete()
+
+    def test_create_new_product(self):
+        self.assertTrue(self.product is not None)
+        self.assertEqual(1, Product.objects.count())
 
     def test_str_name_repr(self):
-        entry = Category(name='Category 1')
-        self.assertEqual(str(entry), entry.name)
+        self.assertEqual(str(self.product), self.product.name)
 
     def test_instance_product(self):
-        product = Product.objects.create(
-            name='Product 1',
-            description='Testing Model Products',
-            price=100
-        )
-        self.assertIsInstance(product, Product)
+        self.assertIsInstance(self.product, Product)
