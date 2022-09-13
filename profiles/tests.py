0fb38ad0618680255d301a483b471ccd58a9edb1
Emidio Valeretto <emidio.valereto@gmail.com>
2022-09-13 10:59:28 +0000

    Refactor profiles tests.

--- profiles/tests.py
+++ profiles/tests.py
@@ -17,15 +17,10 @@ class TestModelsUser(TestCase):
         self.user.delete()
 
     def test_create_new_user(self):
-        user = User(
-            username='test',
-            password='12test12',
-            email='test@test.com'
-        )
-        self.assertTrue(user)
-        self.assertIsInstance(user, User)
+        self.assertTrue(self.user)
+        self.assertIsInstance(self.user, User)
         self.assertEqual(1, User.objects.count())
-        self.assertIsNotNone(user)
+        self.assertIsNotNone(self.user)
 
 
 class TestSignin(TestCase):
@@ -42,8 +37,7 @@ class TestSignin(TestCase):
         self.user.delete()
 
     def test_user_is_authenticated_must_return_true(self):
-        user = User(username='test', password='12test12')
-        self.assertTrue((user is not None) and user.is_authenticated)
+        self.assertTrue((self.user is not None) and self.user.is_authenticated)
 
     def test_user_signin_with_wrong_username_must_return_false(self):
         user = authenticate(username='test_wrong_username',
@@ -70,19 +64,10 @@ class TestViewAuthentication(TestCase):
 
     def test_user_exists_must_return_200(self):
         context = {
-            'username': 'test',
-            'password': '12test12'
+            'username': self.user.username,
+            'password': self.user.password
         }
 
         response = self.client.post('/auth/login/', data=context)
         self.assertTrue(response)
         self.assertEqual(200, response.status_code)
-
-    def test_user_not_found_must_return_404(self):
-        context = {
-            'username': 'anonymous',
-            'password': '12anonymous12'
-        }
-        response = self.client.post('/auth/login/', data=context)
-        self.assertFalse(response)
-        self.assertEqual(404, response.status_code)
