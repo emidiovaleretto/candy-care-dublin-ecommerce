f67e5ee59c8764cffade9616f2bc096d565a4ce1
Emidio Valeretto <emidio.valereto@gmail.com>
2022-09-18 14:38:58 +0000

    Refactor code as per PEP8.

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,8 +1,7 @@
 
-from django.shortcuts import (render,
-                              redirect,
-                              get_object_or_404)
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+
 from products.models import Product
 
 
