db749b4ef838283dc9bc05a200a976ea6c68f563
Emidio Valeretto <emidio.valereto@gmail.com>
2022-09-23 13:34:44 +0000

    Create a model to add a occasion to a product.

---
+++ products/models/Models_Occasion.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Occasion(models.Model):
+    """
+    This class creates a occasion
+    e.g. mothers_day, christmas
+    """
+
+    class Meta:
+        verbose_name_plural = 'Occasions'
+
+    name = models.CharField(max_length=60)
+    friendly_name = models.CharField(max_length=80, null=True, blank=True)
+
+    def __str__(self):
+        """
+        This method when called returns readable occasion name.
+        """
+        return self.name
+
+    def get_friendly_name(self):
+        """
+        This method when called returns the friendly name of the occasion.
+        e.g. Mother's Day, Christmas
+        """
+        return self.friendly_name
--- products/models/__init__.py
+++ products/models/__init__.py
@@ -2,3 +2,4 @@ from django.db import models
 
 from .Models_Product import Product
 from .Models_Category import Category
+from .Models_Occasion import Occasion
