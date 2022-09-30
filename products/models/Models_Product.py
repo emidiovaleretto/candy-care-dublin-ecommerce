510c9654577e3a0c1954d5e7e1ee8a89a753db22
Emidio Valeretto <emidio.valereto@gmail.com>
2022-09-30 18:50:08 +0000

    Modify Product models.

--- products/models/Models_Product.py
+++ products/models/Models_Product.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from products.models import *
 
 
@@ -6,14 +7,15 @@ class Product(models.Model):
     This class is used to create a product.
     """
 
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    occasion = models.ForeignKey('Occasion', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    occasion = models.ForeignKey(
+        'Occasion', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=254)
+    slug = models.SlugField(max_length=100, null=False, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
