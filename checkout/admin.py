90f5af550e42e8d183ec7794f56b1cc0d2fd05cc
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-04 18:47:37 +0000

    Customize admin order panel.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+        'original_cart',
+        'stripe_payment_id',
+    )
+
+    fields = (
+        'order_number',
+        'user_profile',
+        'date',
+        'full_name',
+        'email',
+        'phone_number',
+        'country',
+        'postcode',
+        'town_or_city',
+        'street_address_1',
+        'street_address_2',
+        'county',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+        'original_cart',
+        'stripe_payment_id',
+    )
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'order_total',
+        'delivery_cost',
+        'grand_total',
+    )
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
