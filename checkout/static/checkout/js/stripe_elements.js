20d5d3b80f7fdada562708601a822893a90d3bb0
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-05 17:30:54 +0000

    Create initial structure of checkout functionality.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -28,3 +28,51 @@ let card = elements.create('card', {
 });
 
 card.mount('#card-element');
+
+// The function below handles realtime validation errors on the card element
+
+card.addEventListener('change', function (event) {
+    let errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+let form = document.getElementById('payment-form');
+
+// The function below handles the form submition
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import (render, redirect,
+                              reverse, get_object_or_404)
+from django.contrib import messages
 
-# Create your views here.
+from django.conf import settings
+from django.views.decorators.http import require_POST
+
+from django.http import HttpResponse
+from .forms import OrderForm
+
+from .models import Order, OrderLineItem
+from products.models import Product
+
+from profiles.models.Models_Profile import UserProfile
+from profiles.forms import UserProfileForm
+
+from shopping_bag.contexts import shopping_bag_contents
+
+import stripe
+import json
+
+
+def checkout(request):
+
+    stripe_publishable_key = settings.STRIPE_PUBLISHABLE_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    order_form = OrderForm()
+
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    if not shopping_bag:
+        messages.error(
+            request, "There's nothing in your bag at the moment.")
+        return redirect(reverse('products'))
+
+    current_bag = shopping_bag_contents(request)
+    total = current_bag['grand_total']
+
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+
+    payment_intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
+    if not stripe_publishable_key:
+        messages.warning(
+            request,
+            'Stripe public key is missing. '
+            'Did you forget to set it in your environment?'
+        )
+
+    context = {
+        'order_form': order_form,
+        'stripe_publishable_key': stripe_publishable_key,
+        'client_secret': payment_intent.client_secret,
+    }
+
+    return render(request, 'checkout/checkout.html', context=context)
