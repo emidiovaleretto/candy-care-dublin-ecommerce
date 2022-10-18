76e3f6df5b9fb00f28ef981778ee9e397cf53c4e
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-18 14:42:42 +0000

    Set EMAIL_HOST_PASSWORD envvar.

--- candycaredublin/settings/settings.py
+++ candycaredublin/settings/settings.py
@@ -197,5 +197,5 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
