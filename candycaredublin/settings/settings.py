16e69565330c2421b90807c4f283e2afa4872c3d
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-18 03:50:05 +0000

    Fix EMAIL_HOST_PASSWORD envvar.

--- candycaredublin/settings/settings.py
+++ candycaredublin/settings/settings.py
@@ -193,9 +193,9 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'candycaredublin@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-    EMAIL_PORT = os.environ.get('EMAIL_PORT')
-    EMAIL_HOST = os.environ.get('EMAIL_HOST')
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
