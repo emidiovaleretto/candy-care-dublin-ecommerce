4bc3a9128f64466e1a6dd2780f2c2871d603edd8
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-27 12:09:27 +0000

    Fix a typo.

--- products/forms.py
+++ products/forms.py
@@ -10,12 +10,11 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
-        super().__init__(**args, **kwargs)
+        super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(category.id, category.friendly_name())
+        friendly_names = [(category.id, category.get_friendly_name())
                           for category in categories]
 
         self.fields['category'].choices = friendly_names
-
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
