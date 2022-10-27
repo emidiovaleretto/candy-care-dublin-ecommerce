5234a0a7f5971bf7e902ba8692d4629ba82e7c7f
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-27 10:38:37 +0000

    Customize Admin Panel.

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models.Models_Newsletter import Newsletter
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_email',
+        'created_at'
+    )
+    search_fields = ('user_email',)
+
+admin.site.register(Newsletter, NewsletterAdmin)
