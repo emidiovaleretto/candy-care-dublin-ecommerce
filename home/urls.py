4a7922014b0a024dbbbc639afc5741335f635503
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-27 09:20:30 +0000

    Remove success function from home app.

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('success/', views.success, name='success'),
     path('privacy/', views.privacy, name='privacy'),
 ]
