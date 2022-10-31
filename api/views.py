dbf0bf6ba95fe380ccd7a6c3e6e48655405b630d
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-31 02:31:47 +0000

    Create a method to edit a product and save it in the store database.

--- api/views.py
+++ api/views.py
@@ -44,3 +44,53 @@ def add_product(request):
         'form': form,
     }
     return render(request, 'api/add_product.html', context=context)
+
+
+@login_required(login_url='/auth/login')
+def edit_all_products(request):
+    """
+    This method loads a list of
+    all products available for editing.
+    """
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'api/edit_products.html', context=context)
+
+
+@login_required(login_url='/auth/login')
+def edit_product(request, product_id):
+    """
+    This method edits a product in the store.
+    """
+    if not request.user.is_superuser:
+        messages.error(request,
+                       'It appears you tried to access a page that you do not have permission. \
+                        Please contact the store owner for assistance.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'{product.name} updated successfully!')
+            return redirect(reverse('product_detail', args=[product.slug]))
+        else:
+            messages.error(request,
+                           'Something went wrong while trying to update your product. \
+                            Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing [{product.name}]')
+
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, 'api/edit_product.html', context=context)
